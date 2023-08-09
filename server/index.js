19aee769ec9c5365a6a9179cfff307d0e4122f6a
Aazib Chaudhry <aazibch47@gmail.com>
2023-08-09 07:14:28 +0500

    Set up /authenticate route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -7,6 +9,24 @@ app.use(cors({ origin: true }));
 
 app.post('/authenticate', async (req, res) => {
   const { username } = req.body;
+
+  try {
+    const response = await axios.put(
+      'https://api.chatengine.io/users/',
+      {
+        username,
+        secret: username,
+        firstName: username
+      },
+      { headers: { 'PRIVATE-KEY': process.env.CHAT_ENGINE_PRIVATE_KEY } }
+    );
+
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    console.log('error', error);
+    return res.status(error.response.status).json(error.response.data);
+  }
+
   return res.json({ username: username, secret: 'sha256...' });
 });
 
