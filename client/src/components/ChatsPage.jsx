f1fcc5e01edeb90f370be847bdea0caa7faca8db
Aazib Chaudhry <aazibch47@gmail.com>
2023-08-11 16:02:24 +0500

    Switch to react-chat-engine-pretty package for the UI

--- client/src/components/ChatsPage.jsx
+++ client/src/components/ChatsPage.jsx
@@ -1,20 +1,14 @@
-import {
-  MultiChatSocket,
-  MultiChatWindow,
-  useMultiChatLogic
-} from 'react-chat-engine-advanced';
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
 const ChatsPage = (props) => {
-  const chatProps = useMultiChatLogic(
-    import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID,
-    props.user.username,
-    props.user.secret
-  );
-
   return (
     <div style={{ height: '100vh' }}>
-      <MultiChatSocket {...chatProps} />
-      <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+      <PrettyChatWindow
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: '100vh' }}
+      />
     </div>
   );
 };
